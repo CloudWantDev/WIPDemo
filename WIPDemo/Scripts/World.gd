13524e06e23125ff33337559c710962c6bbebf1d
CloudWantDev <Shayneclarkjob@gmail.com>
2022-10-04 10:57:57 -0400

    idek

---
+++ WIPDemo/Scripts/World.gd
@@ -0,0 +1,32 @@
+extends Node2D
+
+onready var tween := $Tween
+onready var Player := $Player
+onready var PlayerMiddlePosition := $PlayerMiddlePosition
+var TransitionTime := 1
+
+func _ready():
+	yield(get_tree().create_timer(5),"timeout")
+	Player.get_node("PlayerSprites").play("Run")
+	Player.position.x = -12.5
+	tween.interpolate_property(Player,"position",null,Vector2(50,138),TransitionTime)
+	tween.start()
+
+func _move_player_in():
+	TransitionTime = 6
+	Player.get_node("PlayerSprites").play("Run")
+	tween.interpolate_property(Player,"position",null,PlayerMiddlePosition.position,TransitionTime)
+	tween.start()
+
+func _move_player_out():
+	TransitionTime = 6
+	tween.interpolate_property(Player,"position",null,Vector2(-25,138),TransitionTime)
+	tween.start()
+
+
+
+
+func _on_Tween_tween_completed(object, key):
+	if object == Player and key == ":position":
+		print("Idle")
+		Player.get_node("PlayerSprites").play("IdleNoLook")
